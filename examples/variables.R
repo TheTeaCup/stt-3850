6eb722477b936df400934951824218c0998be90e
TheTeaCup <lw0482352@gmail.com>
2024-01-18 20:45:34 -0500

    variables

---
+++ examples/variables.R
@@ -0,0 +1,21 @@
+# Assign the value 42 to x
+x <- 42
+
+# Print out the value of the variable x
+x
+
+# Assign a value to the variables my_apples and my_oranges
+my_apples <- 5
+my_oranges <- 6
+
+# Add these two variables together
+my_apples + my_oranges
+
+# Create the variable my_fruit
+my_fruit <- my_apples + my_oranges
+
+# String
+my_character <- "universe"
+
+# Change my_logical to be FALSE
+my_logical <- FALSE
