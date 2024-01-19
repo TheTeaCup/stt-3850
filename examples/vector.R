d2b3f082cb5dfb2bec16062f6ba6520ba808bee9
TheTeaCup <lw0482352@gmail.com>
2024-01-18 20:45:40 -0500

    vector

---
+++ examples/vector.R
@@ -0,0 +1,55 @@
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector
+total_vector <-  A_vector + B_vector
+  
+# Print out total_vector
+total_vector
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Total winnings with poker
+total_poker <- sum(poker_vector)
+
+# Total winnings with roulette
+total_roulette <-  sum(roulette_vector)
+
+# Total winnings overall
+total_week <- total_roulette + total_poker
+
+# Print out total_week
+total_week
+
+# Define a new variable based on a selection
+poker_wednesday <- poker_vector[3]
+
+# Define a new variable based on a selection
+poker_midweek <- poker_vector[c(2, 3, 4)]
+
+# Select poker results for Monday, Tuesday and Wednesday
+poker_start <- poker_vector[c("Monday", "Tuesday", "Wednesday")]
+  
+# Calculate the average of the elements in poker_start
+mean(poker_start)
+
+# Sex vector
+sex_vector <- c("Male", "Female", "Female", "Male", "Male")
+
+# Convert sex_vector to a factor
+factor_sex_vector <- factor(sex_vector)
+
+# Print out factor_sex_vector
+factor_sex_vector
+
+#       OUTPUT
+#[1] Male   Female Female Male   Male  
+#Levels: Female Male
