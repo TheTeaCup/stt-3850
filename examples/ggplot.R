7afe7a06bd74a9afd32397d12aa4f770f4ac79cb
TheTeaCup <lw0482352@gmail.com>
2024-01-31 11:10:03 -0500

    ggplot2

---
+++ examples/ggplot.R
@@ -0,0 +1,14 @@
+hist(mtcars$mpg)
+barplot(table(x))
+boxplot(mtcars$mpg)
+summary(mtcars$mpg)
+plot(mtcars$wt, mtcars$mpg)
+
+library(ggplot2)
+
+ggplot(data=mtcars, mapping=aes())
+ggplot(data=mtcars, mapping=aes(x=wt, y=mpg))
+ggplot(data=mtcars, 
+    mapping = aes(x = wt, y = mpg), 
+    ylab("Miles/(US) gallon"), 
+    xlab("Weight (1000 lbs)"))
