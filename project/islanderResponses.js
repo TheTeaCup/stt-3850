5aab6a8c52bfc2976ee2c00c6495b7f9a999c0e0
TheTeaCup <lw0482352@gmail.com>
2024-04-03 22:55:40 -0400

    people are sleeping

--- project/islanderResponses.js
+++ project/islanderResponses.js
@@ -4,7 +4,40 @@ require("dotenv").config({
   path: "./.env",
 });
 
-const islanderData = require("./islands/ProvidenceIslanders.json");
+const islanderData = require("./islands/BonneSanteIslanders.json");
+let questions = [
+  "Are you male or female?",
+  "How many years old are you?",
+  "How tall are you in centimetres?",
+  "How many children do you have?",
+  "How many times have you married?",
+  "How many villages have you lived in?",
+  "When is your birthday?",
+  "Which village were you born in?",
+  "Which year were you born in?",
+  "On a scale from 1 to 10, how attractive do you think you are to members of the opposite sex?",
+  "What is the predominant colour of your eyes",
+  "How many hours did you sleep last night?",
+  "How many minutes did you spend doing low intensity physical activity in the last seven days?",
+  "How many minutes did you spend doing moderate physical activity in the last seven days?",
+  "How many minutes did you spend doing vigorous physical activity in the last seven days?",
+  "How much do you weigh in kilograms?",
+  "What is your pulse rate while you are completing this survey?",
+  "Do you eat meat?",
+  "How many serves of fruit do you usually eat each day?",
+  "How many serves of vegetables do you usually eat each day?",
+  "Compared with one year ago, how would you rate your health in general now?",
+  "Have you ever had a coronary heart event?",
+  "How often do you currently smoke cigarettes?",
+  "On a scale from 1 to 10, how anxious do you feel right now?",
+  "On a scale from 1 to 10, how depressed do you feel right now?",
+  "What is your favourite music genre?",
+  "What musical instruments do you play?",
+  "Are you currently breastfeeding?",
+  "What do you think your IQ is on the adult intelligence scale?",
+  "What is your favourite pizza topping?",
+  "Which superpower would you most like to have?",
+];
 
 async function islanderResponses() {
   const browser = await puppeteer.launch({
@@ -66,12 +99,22 @@ async function islanderResponses() {
           response: uniqueResponses[index],
         }));
       });
+      const filteredData = questions
+        .filter((question) =>
+          surveyResults.some((result) => result.question === question)
+        )
+        .map((question) => ({
+          question,
+          response: surveyResults.find((result) => result.question === question)
+            ?.response,
+        }));
 
-      islanderData[i].responses = surveyResults;
+
+      islanderData[i].responses = filteredData;
 
       // Write the updated data back to the JSON file
       fs.writeFileSync(
-        "islands/ProvidenceIslanders-surveyed.json",
+        "islands/BonneSanteIslanders-surveyed.json",
         JSON.stringify(islanderData, null, 2)
       );
     }
--- project/surveyIslander.js
+++ project/surveyIslander.js
@@ -4,7 +4,7 @@ require("dotenv").config({
   path: "./.env",
 });
 
-const islanderData = require("./islands/BonneSanteIslanders.json");
+const islanderData = require("./islands/IronBardIslanders.json");
 
 async function delay(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
